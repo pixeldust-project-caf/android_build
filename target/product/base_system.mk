cf451919c6100f90c562dfa2a66fe2ea87bb52e9
Roopa Sattiraju <sattiraju@google.com>
2022-02-03 09:04:07 -0800

    Enabling Apex in BT
    Bug: 217736913
    Test: Compile, bringup and test user journeys
    Change-Id: I2371a3cfbb3770802b1a318cf0695af52dcf8cc0
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
     com.android.adbd \
     com.android.adservices \
     com.android.appsearch \
+    com.android.bluetooth \
     com.android.configinfrastructure \
     com.android.conscrypt \
     com.android.devicelock \
