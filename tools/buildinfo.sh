c1fb4d82b47a85b0298a73e73f41427e43ad67b4
spezi77 <spezi7713@gmx.net>
2022-04-09 14:51:06 +0200

    Revert "build: set custom fingerprint in ro.build.fingerprint too"
    This reverts commit dd7d4bb5c6e6f829152d24c3988b5fc223d6bc88.
    
    Reason: Caused a conflict when using static sysprops in device tree.
    
    error: found duplicate sysprop assignments
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ie87f9751642d744e9c7944158e40a1eeef8dc32b

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -49,8 +49,7 @@ echo "ro.wifi.channels=$PRODUCT_DEFAULT_WIFI_CHANNELS"
 echo "# ro.build.product is obsolete; use ro.product.device"
 echo "ro.build.product=$TARGET_DEVICE"
 
-echo "# Do not try to parse fingerprint or thumbprint"
-echo "ro.build.fingerprint=$BUILD_FINGERPRINT"
+echo "# Do not try to parse thumbprint"
 echo "ro.build.description=$PRIVATE_BUILD_DESC"
 if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
