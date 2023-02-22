9f6eae40ad66fe6ccd25f36f453904f9c19f0f7b
spezi77 <spezi7713@gmx.net>
2023-02-22 14:59:31 +0100

    core: Change path for new board config in our vendor.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/config.mk
+++ core/config.mk
@@ -360,7 +360,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 ifneq ($(PIXELDUST_BUILD),)
-include vendor/pixeldust/config/BoardConfigPD.mk
+include vendor/pixeldust/build/board/BoardConfigPD.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
