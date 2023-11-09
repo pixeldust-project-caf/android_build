GrapheneOS/platform_build

ignore tethering provisioning requirement

core/main.mk
@@ -267,6 +267,7 @@ ifneq (,$(user_variant))
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
   ADDITIONAL_DEFAULT_PROPERTIES += ro.control_privapp_permissions=enforce
   ADDITIONAL_DEFAULT_PROPERTIES += persist.security.deny_new_usb=dynamic
+  ADDITIONAL_DEFAULT_PROPERTIES += net.tethering.noprovisioning=true
 
   ifeq ($(user_variant),user)
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1

pixeldust-project-caf/android_build

Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
Signed-off-by: spezi77 <spezi7713@gmx.net>

core/main.mk
@@ -372,6 +372,7 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_SYSTEM_PROPERTIES += ro.secure=1
   ADDITIONAL_SYSTEM_PROPERTIES += security.perf_harden=1
+  ADDITIONAL_SYSTEM_PROPERTIES += net.tethering.noprovisioning=true
 
   ifeq ($(user_variant),user)
     ADDITIONAL_SYSTEM_PROPERTIES += ro.adb.secure=1
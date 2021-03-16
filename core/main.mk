6bb21cac741eee940f612584e607b40b3753fdd8
Daniel Micay <danielmicay@gmail.com>
2021-03-16 02:05:40 -0400

    ignore tethering provisioning requirement
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/main.mk
+++ core/main.mk
@@ -372,6 +372,7 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_SYSTEM_PROPERTIES += ro.secure=1
   ADDITIONAL_SYSTEM_PROPERTIES += security.perf_harden=1
+  ADDITIONAL_SYSTEM_PROPERTIES += net.tethering.noprovisioning=true
 
   ifeq ($(user_variant),user)
     ADDITIONAL_SYSTEM_PROPERTIES += ro.adb.secure=1
